6095d70d15c1fa75edec1c7fc79ef9f1c613ea5d
Derek Pollard <dpollard@mail.bradley.edu>
2022-05-24 13:39:54 -0500

    Add in problem info

--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -9,6 +9,14 @@ export function HomePage() {
         <title>HomePage</title>
         <meta name="description" content="A Boilerplate application homepage" />
       </Helmet>
+      <p>
+        To replicate the issue; drag the clips resize handler to the left then
+        back and watch how the action stops being active/fired.
+      </p>
+      <p>
+        I've added the <code>console.log</code> for the gesture's active, state
+        (and others)
+      </p>
       <TrackView />
     </>
   );
